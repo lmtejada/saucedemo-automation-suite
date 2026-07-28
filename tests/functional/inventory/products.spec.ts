6155808706f33c4d7ab9660e17030de66f88bfc5
Laura Tejada <laura_mtc@hotmail.com>
2026-07-28 09:32:03 +0200

    reset cart state in inventory tests

--- tests/functional/inventory/products.spec.ts
+++ tests/functional/inventory/products.spec.ts
@@ -5,8 +5,10 @@ import { sortProductsByName, sortProductsByPrice } from '@utils/products';
 import INVENTORY_PRODUCTS from '@test-data/static/products.json';
 
 test.describe('inventory feature', () => {
-    test.beforeEach(async ({ inventoryPage }) => {
+    test.beforeEach(async ({ page, inventoryPage, resetCart }) => {
         await inventoryPage.open();
+        await resetCart();
+        await page.reload();
     });
 
     test.describe('functional tests @regression', () => {
